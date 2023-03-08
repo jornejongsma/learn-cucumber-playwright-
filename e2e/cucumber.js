7fdbb581a6e3d0388b01759f68e8ac6045bf9911
jorne <jorne.jongsma@car-go.nl>
2023-03-08 22:07:56 +0100

    without babel

--- e2e/cucumber.js
+++ e2e/cucumber.js
@@ -1,17 +1,18 @@
-// const os = require('os')
-//
-// const cpuCount = os.cpus().length
-// const cpuCountUsed = cpuCount / 2
-//
-// console.log(`Running on ${cpuCountUsed} of ${cpuCount} cores.`)
-//
-// const common = `
-//     --require-module ts-node/register
-//     --require src/step-definitions/**/**/*.ts
-// `
-//
-// module.exports = {
-//     default: `${common}`
-// }
+const os = require('os')
 
-module.exports = require('./dist')
+const cpuCount = os.cpus().length
+const cpuCountUsed = cpuCount / 2
+
+console.log(`Running on ${cpuCountUsed} of ${cpuCount} cores.`)
+
+const common = `./src/features/**/*.feature \
+                --require-module ts-node/register \
+                --require src/step-definitions/**/**/*.ts \
+                -f json: ./reports/report.json \
+                --format progress-bar`;
+
+const dev = `${common} --tags '@dev'`;
+const smoke = `${common} --tags '@smoke'`;
+const regression = `${common} --tags '@regression'`;
+
+module.exports = { dev, smoke, regression}
